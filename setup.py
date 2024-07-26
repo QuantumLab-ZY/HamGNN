202307c815545486ecd0269c2305b6fb8bd38997
Yang Zhong <59390411+QuantumLab-ZY@users.noreply.github.com>
2024-07-27 00:00:02 +0800

    fix a small bug in setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
         "console_scripts": [
             "HamGNN = HamGNN.main:HamGNN",
             "band_cal = utils_openmx.band_cal:main",
-            "graph_data_gen = utils_openmx.graph_data_gen:main"，
+            "graph_data_gen = utils_openmx.graph_data_gen:main",
             "poscar2openmx = utils_openmx.poscar2openmx:main"
         ]
     },
